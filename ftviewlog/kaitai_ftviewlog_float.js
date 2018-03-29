6a8fde08a68f0a9204d6fb2a9b9d808cc0b51f5b
eugene vikulov <eugene@vikulov.com>
2018-03-29 16:21:37 +0700

    аа

--- ftviewlog/kaitai_ftviewlog_float.js
+++ ftviewlog/kaitai_ftviewlog_float.js
@@ -1,9 +1,23 @@
+// This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['kaitai-struct/KaitaiStream'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('kaitai-struct/KaitaiStream'));
+  } else {
+    root.FtviewFloatlog = factory(root.KaitaiStream);
+  }
+}(this, function (KaitaiStream) {
 var FtviewFloatlog = (function() {
   function FtviewFloatlog(_io, _parent, _root) {
     this._io = _io;
     this._parent = _parent;
     this._root = _root || this;
 
+    this._read();
+  }
+  FtviewFloatlog.prototype._read = function() {
     this.header = new TableHeaderT(this._io, this, this._root);
     this.rows = new Array(this.header.numRecords);
     for (var i = 0; i < this.header.numRecords; i++) {
@@ -17,6 +31,9 @@ var FtviewFloatlog = (function() {
       this._parent = _parent;
       this._root = _root || this;
 
+      this._read();
+    }
+    TableHeaderT.prototype._read = function() {
       this.version = this._io.ensureFixedContents([3]);
       this.lastUpdateYear = this._io.readU1();
       this.lastUpdateMonth = this._io.readU1();
@@ -36,6 +53,9 @@ var FtviewFloatlog = (function() {
       this._parent = _parent;
       this._root = _root || this;
 
+      this._read();
+    }
+    FloatRowT.prototype._read = function() {
       this.date = KaitaiStream.bytesToStr(this._io.readBytes(9), "ASCII");
       this.time = KaitaiStream.bytesToStr(this._io.readBytes(8), "ASCII");
       this.millitm = KaitaiStream.bytesToStr(this._io.readBytes(3), "ASCII");
@@ -46,20 +66,26 @@ var FtviewFloatlog = (function() {
       this.internal = this._io.readS4le();
     }
 
+    /**
+     *  YYYY-MM-DD
+     */
+
+    /**
+     * HH-MM-SS
+     */
+
+    /**
+     * DDD
+     */
+
+    /**
+     * DDDDD
+     */
+
     return FloatRowT;
   })();
 
   return FtviewFloatlog;
 })();
-
-// Export for amd environments
-if (typeof define === 'function' && define.amd) {
-  define('FtviewFloatlog', [], function() {
-    return FtviewFloatlog;
-  });
-}
-
-// Export for CommonJS
-if (typeof module === 'object' && module && module.exports) {
-  module.exports = FtviewFloatlog;
-}
+return FtviewFloatlog;
+}));
